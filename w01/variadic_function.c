8425584c970605181967e03f9bd279b7c7d80070
iusyu <iuamyy@163.com>
2021-07-11 18:14:14 +0800

    variadic function add node

--- w01/variadic_function.c
+++ w01/variadic_function.c
@@ -9,6 +9,18 @@ typedef struct _node {
 }node;
 
 
+int init_list(node** head)
+{
+	if( ((*head) = calloc(1, sizeof(node)))==NULL )
+	{
+		perror("malloc failure");
+		exit(EXIT_FAILURE);
+	}
+
+	(*head)->next = NULL;
+	(*head)->data = -1;
+}
+
 int add_node(node** head, int data)
 {
 	if( !(head != NULL && *head != NULL) ){
@@ -22,14 +34,38 @@ int add_node(node** head, int data)
 	}
 
 	tmp->next = (*head)->next;
+	tmp->data = data;
 	(*head)->next = tmp;
 	return 0;
 }
 
 int add_N_node(node** head,...)
 {
-	va_list ap;
-	for( 
+	va_list vl;
+	va_start(vl, head);
+	int i = 0;
+	for( ; i != -1 ; i = va_arg(vl, int)){
+		add_node(head, i);
+	}
+	va_end(vl);
 }
 
+
+
+void test_VARIABLE_PARAMETER(){
+	node* head = NULL;
+	init_list(&head);
+	add_N_node(&head, 1,3,4,5,6,7,7,-1);
+	printf("%d ", head->data);
+	printf("%d ", head->next->data);
+	printf("%d ", head->next->next->data);
+	printf("%d ", head->next->next->next->data);
+	printf("%d ", head->next->next->next->next->data);
+}
+
+
+int main(int argc, char* argv[] )
+{
+	test_VARIABLE_PARAMETER();
+}
 
